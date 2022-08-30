e20e9cc140d843b0e9bf894599bddec4748197c1
simpson9955 <gtnsimpson@gmail.com>
2022-08-30 16:15:53 +0100

    lesson complete

--- myApp.js
+++ myApp.js
@@ -1,39 +1,56 @@
-let express = require('express');
+/** @format */
+
+//require("dotenv").config();
+let bodyParser = require("body-parser");
+let express = require("express");
 let app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use("/public", express.static(__dirname + "/public"));
 
+console.log("hello world");
 
+app.get("/", function (req, res) {
+	res.sendFile(__dirname + "/views/index.html");
+});
 
+app.get("/json", function (req, res) {
+	if (process.env.MESSAGE_STYLE === "uppercase") {
+		res.json({ message: "HELLO JSON" });
+	} else {
+		res.json({ message: "Hello json" });
+	}
+});
 
+app.use("/", function (req, res, next) {
+	console.log(req.method + " " + req.path + " - " + req.ip);
+	next();
+});
 
+app.get(
+	"/now",
+	function (req, res, next) {
+		req.time = new Date().toString();
+		next();
+	},
+	function (req, res) {
+		res.json({ time: req.time });
+	}
+);
 
+app.get("/:word/echo", function (req, res) {
+	res.json({ echo: req.params.word });
+});
 
+app
+	.route("/name")
+	.get(function (req, res) {
+		res.json({ name: req.query.first + " " + req.query.last });
+	})
+	.post(function (req, res) {
+		let nameStr = req.body.first + " " + req.body.last;
+		res.json({ name: nameStr });
+	});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
